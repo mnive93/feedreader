2f48bfdef4cdee1f67c65e8ab7e2a3f6f5a71fc7
Nivedita Muthusubramanian <nivedita-2608@nivedita-2608.local>
2015-01-19 23:48:24 +0530

    added filter

--- js/app.js
+++ js/app.js
@@ -1,40 +1,49 @@
-var feeds = [];
-angular.module('feedReader',['ngResource'])
-       
-        .factory('FeedFetcher',function($resource) {
-        return $resource('http://ajax.googleapis.com/ajax/services/feed/load', {}, {
-			fetch: { method: 'JSONP', params: {v: '1.0', callback: 'JSON_CALLBACK'} }
-		}); 
-        })
-           
-        .service('FeedList',function($rootScope,FeedFetcher) {
-            this.get = function() {
-                var sourcefeeds = [
-                    {title: 'NDTV', url: 'http://feeds.feedburner.com/NdtvNews-TopStories?format=xml'},
-                ];
-            
-            if(feeds.length === 0)
-            {
-                for(var i=0 ;i<sourcefeeds.length;i++)
+    var feeds = [];
+    var sourcefeeds = [];
+    angular.module('feedReader',['ngResource'])
+
+            .factory('FeedFetcher',function($resource) {
+            return $resource('http://ajax.googleapis.com/ajax/services/feed/load', {}, {
+                fetch: { method: 'JSONP', params: {v: '1.0', callback: 'JSON_CALLBACK'} }
+            }); 
+            })
+
+            .service('FeedList',function($rootScope,FeedFetcher) {
+                this.get = function() {
+                   sourcefeeds = [
+                        {title: 'NDTV', url: 'http://feeds.feedburner.com/NdtvNews-TopStories?format=xml'},
+                        {title: 'TECHCRUNCH', url : 'http://feeds.feedburner.com/techcrunch/startups?format=xml'},
+
+
+                    ];
+
+                if(feeds.length === 0)
                 {
-                    FeedFetcher.fetch({q: sourcefeeds[i].url, num: 10}, {}, function (data) {
-						var feed = data.responseData.feed;
-						feeds.push(feed);
-					});
-                
-                }
-            } 
-                return feeds;
-            };
-        })
+                    for(var i=0 ;i<sourcefeeds.length;i++)
+                    {
+                        FeedFetcher.fetch({q: sourcefeeds[i].url, num: 10}, {}, function (data) {
+                            var feed = data.responseData.feed;
+                            feeds.push(feed);
+                        });
+
+                    }
+                } 
+                    return feeds;
+                };
+            })
 
 
-        .controller('FeedParser',function($scope,FeedList) {
-           $scope.feeds = FeedList.get();
-            console.log($scope.feeds);
-            $scope.$on('FeedList',function(event,data)
-                       {
-                           $scope.feeds = data;
-                       });    
-                   
-        });
+            .controller('FeedParser',function($scope,FeedList) {
+
+               $scope.feeds = FeedList.get();
+                console.log($scope.feeds);
+                $scope.$on('FeedList',function(event,data)
+                           {
+                               $scope.feeds = data;
+                           });    
+                       $scope.sourcefeeds = sourcefeeds;
+  
+            }
+
+
+    );
