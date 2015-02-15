f494e543ffe5efb9cbe196c779d47bc6ef7600ef
Nivedita Muthusubramanian <nivedita-2608@nivedita-2608.local>
2015-02-15 19:34:54 +0530

    minor changes

--- js/app.js
+++ js/app.js
@@ -1,27 +1,24 @@
- var feeds = [];
-    var sourcefeeds = [];
-    angular.module('feedReader',['ngResource'])
-
-            .factory('FeedFetcher',function($resource) {
+ (function() {
+    var app = angular.module('feedReader',['ngResource']);
+    var feeds = [];
+    var sources = [{title: 'NDTV', url: 'http://feeds.feedburner.com/NdtvNews-TopStories?format=xml'},
+                 {title: 'TECHCRUNCH', url : 'http://feeds.feedburner.com/techcrunch/startups?format=xml'},
+                    ];
+    app .factory('FeedFetcher',function($resource) {
             return $resource('http://ajax.googleapis.com/ajax/services/feed/load', {}, {
                 fetch: { method: 'JSONP', params: {v: '1.0', callback: 'JSON_CALLBACK'} }
             }); 
             })
 
-            .service('FeedList',function($rootScope,FeedFetcher) {
+          app.service('FeedList',function($rootScope,FeedFetcher) {
                 this.get = function() {
-                   sourcefeeds = [
-                        {title: 'NDTV', url: 'http://feeds.feedburner.com/NdtvNews-TopStories?format=xml'},
-                        {title: 'TECHCRUNCH', url : 'http://feeds.feedburner.com/techcrunch/startups?format=xml'},
-
-
-                    ];
+               
 
                 if(feeds.length === 0)
                 {
-                    for(var i=0 ;i<sourcefeeds.length;i++)
+                    for(var i=0 ;i<sources.length;i++)
                     {
-                        FeedFetcher.fetch({q: sourcefeeds[i].url, num: 10}, {}, function (data) {
+                        FeedFetcher.fetch({q: sources[i].url, num: 10}, {}, function (data) {
                             var feed = data.responseData.feed;
                             feeds.push(feed);
                         });
@@ -33,13 +30,9 @@
             })
 
 
-            .controller('FeedParser',function($scope,FeedList) {
-                $scope.sourcefeeds = [
-                        {title: 'NDTV', url: 'http://feeds.feedburner.com/NdtvNews-TopStories?format=xml'},
-                        {title: 'TECHCRUNCH', url : 'http://feeds.feedburner.com/techcrunch/startups?format=xml'},
-                    ];
-               $scope.feeds =  FeedList.get();
-      
+         app.controller('FeedParser',function($scope,FeedList) {
+                $scope.sourcefeeds = sources;
+                $scope.feeds =  FeedList.get();
                 $scope.$on('FeedList',function(event,data)
                            {
                                $scope.feeds = data;
@@ -50,3 +43,4 @@
 
 
     );
+}) ();
